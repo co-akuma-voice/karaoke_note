3cfdc1e69310e5cb2d384a4a2930e75770fd401d
Yuya Fujii <koiking213@gmail.com>
2023-06-25 19:55:10 +0900

    画面遷移を追加

--- app/src/main/java/com/example/karaoke_note/MainActivity.kt
+++ app/src/main/java/com/example/karaoke_note/MainActivity.kt
@@ -5,12 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.karaoke_note.ui.theme.Karaoke_noteTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,9 +37,41 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        Greeting("Android")
-                        Greeting("Android")
+                    val navController = rememberNavController()
+                    val canPop = remember { mutableStateOf(false) }
+                    LaunchedEffect(navController) {
+                        navController.addOnDestinationChangedListener { _, _, _ ->
+                            canPop.value = navController.previousBackStackEntry != null
+                        }
+                    }
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = { Text("My App") },
+                                navigationIcon = {
+                                    if (canPop.value) {
+                                        IconButton(onClick = { navController.navigateUp() }) {
+                                            Icon(Icons.Filled.ArrowBack, contentDescription = null)
+                                        }
+                                    }
+                                }
+                            )
+                        }
+                    ) { paddingValues ->
+                        NavHost(navController, startDestination = "home", Modifier.padding(paddingValues)) {
+                            composable("home") {
+                                Column {
+                                    Greeting("Android")
+                                    Greeting("Android")
+                                    Button(onClick = { navController.navigate("song_data") }) {
+                                        Text("Navigate to song_data")
+                                    }
+                                }
+                            }
+                            composable("song_data") {
+                                Greeting("song_data", Modifier.padding(paddingValues))
+                            }
+                        }
                     }
                 }
             }
