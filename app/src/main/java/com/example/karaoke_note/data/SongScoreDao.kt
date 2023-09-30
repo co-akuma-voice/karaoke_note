7ef3cb39d7faedc3488980e7ad915ca2d51f1ee1
Yuya Fujii <koiking213@gmail.com>
2023-10-01 00:05:58 +0900

    全く同じSongScoreは登録しないよう修正

--- app/src/main/java/com/example/karaoke_note/data/SongScoreDao.kt
+++ app/src/main/java/com/example/karaoke_note/data/SongScoreDao.kt
@@ -3,20 +3,40 @@ package com.example.karaoke_note.data
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Transaction
 import kotlinx.coroutines.flow.Flow
+import java.time.LocalDate
 
 @Dao
 interface SongScoreDao {
     @Query("SELECT * FROM SongScore WHERE songId = :songId")
     fun getScoresForSong(songId: Long): Flow<List<SongScore>>
 
-    @Query("SELECT * FROM SongScore ORDER BY date DESC LIMIT 5")
-    fun getLatestScores(): List<SongScore>
+    @Query("SELECT * FROM SongScore ORDER BY date DESC LIMIT :limit")
+    fun getLatestScores(limit: Int): List<SongScore>
+
+    @Query("SELECT * FROM SongScore WHERE songId = :songId AND date = :date AND score = :score AND \"key\" = :key AND comment = :comment")
+    fun findSongScore(songId: Long, date: LocalDate, score: Float, key: Int, comment: String): SongScore?
+
+    @Transaction
+    fun insertSongScore(score: SongScore) {
+        val existingEntry = findSongScore(
+            score.songId,
+            score.date,
+            score.score,
+            score.key,
+            score.comment
+        )
+        if (existingEntry == null) {
+            insertUniqueSongScore(score)
+        }
+    }
 
     @Insert
-    fun insertSongScore(score: SongScore)
+    fun insertUniqueSongScore(songScore: SongScore)
 
     @Query("DELETE FROM SongScore WHERE id = :id")
     fun deleteSongScore(id: Long)
+
 
 }
