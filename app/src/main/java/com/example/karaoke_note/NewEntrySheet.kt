66e818e2470fc7defb19c051b5333a112b903893
yoshi <sub.yoshi.02@gmail.com>
2023-11-12 16:24:30 +0900

    画面回転時におかしくなるので、Spacer() によるスペース確保は止めた。imePadding() を復活。

--- app/src/main/java/com/example/karaoke_note/NewEntrySheet.kt
+++ app/src/main/java/com/example/karaoke_note/NewEntrySheet.kt
@@ -6,10 +6,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -587,10 +585,6 @@ fun NewEntryScreen(navController: NavController, songDao: SongDao, songScoreDao:
                                     )
                                 },
                             )
-                        }
-
-                        item {
-                            Spacer(modifier = Modifier.height(300.dp))
                         }
                     }
                 }
