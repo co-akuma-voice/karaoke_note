0f6fc34f8e45d35361e39f2f46af9b379fbc6406
Yuya Fujii <koiking213@gmail.com>
2023-11-25 18:37:31 +0900

    AppBarにメニューボタンを追加し、FilePickerを表示

--- app/src/main/java/com/example/karaoke_note/AppBar.kt
+++ app/src/main/java/com/example/karaoke_note/AppBar.kt
@@ -15,6 +15,19 @@ import androidx.navigation.NavController
 @Composable
 fun AppBar(navController: NavController) {
     val canPop = remember { mutableStateOf(false) }
+    val showMenu = remember { mutableStateOf(false) }
+
+    val filePickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            val selectedFolderUri = result.data?.data
+            // ログに選択されたファイルのパスを表示
+            Log.d("FolderPicker", "Selected folder: $selectedFolderUri")
+
+        }
+    }
+
     LaunchedEffect(navController) {
         navController.addOnDestinationChangedListener { _, _, _ ->
             canPop.value = navController.previousBackStackEntry != null
