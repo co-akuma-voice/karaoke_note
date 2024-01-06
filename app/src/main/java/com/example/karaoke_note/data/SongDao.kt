af86ab6163b64a9375fc2a13f8d187f5fba781ab
Yuya Fujii <koiking213@gmail.com>
2024-01-06 11:02:44 +0900

    曲リストの表示時に同じ要素を複数個表示しないよう修正

--- app/src/main/java/com/example/karaoke_note/data/SongDao.kt
+++ app/src/main/java/com/example/karaoke_note/data/SongDao.kt
@@ -44,6 +44,6 @@ interface SongDao {
     @Query("DELETE FROM Song")
     fun clearAllSongs()
 
-    @Query("SELECT Song.* FROM Song INNER JOIN SongScore ON Song.id = SongScore.songId WHERE Song.artistId = :artistId")
+    @Query("SELECT Song.* FROM Song INNER JOIN SongScore ON Song.id = SongScore.songId WHERE Song.artistId = :artistId GROUP BY Song.id")
     fun getSongsWithScores(artistId: Long): Flow<List<Song>>
 }
