33fe4f3e53a57a45f80d46aaec222011eb972fb4
yoshi <sub.yoshi.02@gmail.com>
2024-02-17 18:37:18 +0900

    ゲーム選択関数を別ファイルに切り出し。

---
+++ app/src/main/java/com/example/karaoke_note/ui/component/GameKindSelector.kt
@@ -0,0 +1,156 @@
+package com.example.karaoke_note.ui.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.karaoke_note.data.GameKind
+import com.example.karaoke_note.getPainterResourceIdOfBrandImage
+import com.example.karaoke_note.getPainterResourceIdOfGameImage
+
+//
+// NewEntrySheet の採点ゲーム選択部分
+//
+@Composable
+fun ExposedGameSelectorBox(
+    initialGameKind: GameKind,
+    height: Int,
+    modifier: Modifier,    // Card の Modifier
+    isExpanded: Boolean,
+    startPaddingValue: Int,
+){
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.background
+            ),
+            shape = RoundedCornerShape(
+                topStart = 4.dp,
+                topEnd = 4.dp,
+                bottomStart = 0.dp,
+                bottomEnd = 0.dp
+            ),
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height.dp),
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Row(
+                    modifier = Modifier.padding(start = startPaddingValue.dp),
+                    horizontalArrangement = Arrangement.SpaceAround,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Image(
+                        painter = painterResource(
+                            getPainterResourceIdOfBrandImage(
+                                initialGameKind.name.take(3)
+                            )
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.padding(horizontal = 4.dp)
+                    )
+                    Image(
+                        painter = painterResource(
+                            getPainterResourceIdOfGameImage(
+                                initialGameKind.name
+                            )
+                        ),
+                        contentDescription = null,
+                        modifier = Modifier.padding(horizontal = 4.dp)
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                ) {
+                    // Trailing icon の代わり
+                    val arrowIcon: ImageVector = if (isExpanded) {
+                        Icons.Filled.ArrowDropUp
+                    } else {
+                        Icons.Filled.ArrowDropDown
+                    }
+                    Icon(
+                        imageVector = arrowIcon,
+                        contentDescription = null,
+                    )
+                }
+            }
+        }
+        // TextField っぽく見せるために下枠のみ線を入れる
+        Divider(
+            modifier = Modifier.fillMaxWidth(),
+            thickness = 1.dp,
+            color = MaterialTheme.colorScheme.outline
+        )
+    }
+}
+
+@Composable
+fun ExposedGameSelectorItem(
+    gameKind: GameKind,
+    height: Int,
+    textSize: Int,
+    textHorizontalPaddingValues: Int,
+    onClick: () -> Unit
+){
+    DropdownMenuItem(
+        onClick = { onClick() }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(height.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(
+                    getPainterResourceIdOfBrandImage(
+                        gameKind.name.take(3)
+                    )
+                ),
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 4.dp)
+            )
+            Image(
+                painter = painterResource(
+                    getPainterResourceIdOfGameImage(gameKind.name)
+                ),
+                contentDescription = null,
+                modifier = Modifier.padding(horizontal = 4.dp)
+            )
+            Text(
+                text = "(" + gameKind.displayName + ")",
+                fontSize = textSize.sp,
+                modifier = Modifier.padding(start = textHorizontalPaddingValues.dp)
+            )
+        }
+    }
+}
