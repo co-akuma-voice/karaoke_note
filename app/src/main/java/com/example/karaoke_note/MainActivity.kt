e0033ac10fba79819efd93cbe580bda45e070a04
yoshi <sub.yoshi.02@gmail.com>
2024-03-15 23:15:16 +0900

    ArtistsListDrawing() の仮引数から不要になった songScoreDao を削除

--- app/src/main/java/com/example/karaoke_note/MainActivity.kt
+++ app/src/main/java/com/example/karaoke_note/MainActivity.kt
@@ -83,7 +83,7 @@ class MainActivity : ComponentActivity() {
                                 PlansPage(songDao, songScoreDao, artistDao, showDialog, editingSongScore)
                             }
                             composable("list"){
-                                ArtistsPage(navController, artistDao, songDao, songScoreDao)
+                                ArtistsPage(navController, artistDao, songDao)
                             }
                             composable("song_list/{artistId}"){backStackEntry ->
                                 val artistId = backStackEntry.arguments?.getString("artistId")?.toLongOrNull()
