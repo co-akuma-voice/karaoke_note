9c97c13891ee5e65102017a8f33f7dfdd4e47b67
yoshi <sub.yoshi.02@gmail.com>
2024-04-03 15:25:50 +0900

    コメント修正

--- app/src/main/java/com/example/karaoke_note/NewEntrySheet.kt
+++ app/src/main/java/com/example/karaoke_note/NewEntrySheet.kt
@@ -165,7 +165,7 @@ fun entryToDataBase(
     )
     // Plans ページから来た場合は既存の id があるが、マニュアル 0 指定する
     //   0 指定すると新しい番号で振り直される
-    //   Long 型なら 2^63 - 1 (= 9,223,372,036,854,775,807) までいける
+    //   Java の Long 型は 2^63 - 1 (= 9,223,372,036,854,775,807) までいける
     //     毎秒1つ登録しても、使い切るのに 2,924 億年かかるので問題ない
     val newSongScoreId = if (isComeFromPlansPage) { 0L } else { editingSongScore?.id ?: 0L }
     val newSongScore = SongScore(
