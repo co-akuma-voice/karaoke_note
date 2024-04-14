252751798107dae8f9a20b0bcef40e0c892083c0
yoshi <sub.yoshi.02@gmail.com>
2024-04-14 23:54:48 +0900

    まずは基本

--- app/src/main/java/com/example/karaoke_note/AppBar.kt
+++ app/src/main/java/com/example/karaoke_note/AppBar.kt
@@ -7,18 +7,37 @@ import android.provider.DocumentsContract
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.displayCutout
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.FilterAlt
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomSheetDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.runtime.*
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.karaoke_note.data.Artist
 import com.example.karaoke_note.data.ArtistDao
@@ -40,6 +59,7 @@ import java.util.Date
 import java.util.Locale
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppBar(
     navController: NavController,
@@ -49,6 +69,9 @@ fun AppBar(
 ) {
     val canPop = remember { mutableStateOf(false) }
     val showMenu = remember { mutableStateOf(false) }
+
+    var showSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState()
 
     LaunchedEffect(navController) {
         navController.addOnDestinationChangedListener { _, _, _ ->
@@ -66,23 +89,78 @@ fun AppBar(
             }
         },
         actions = {
-            IconButton(onClick = { showMenu.value = true }) {
-                Icon(Icons.Filled.MoreVert, contentDescription = "メニュー")
-            }
-            DropdownMenu(
-                expanded = showMenu.value,
-                onDismissRequest = { showMenu.value = false }
+            Row(
+                modifier = Modifier.align(alignment = Alignment.CenterVertically)
             ) {
-                ImportMenu(songDao, songScoreDao, artistDao, navController.context) {
-                    showMenu.value = false
+                // フィルターボタン
+                IconButton(onClick = { showSheet = true }) {
+                    Icon(
+                        imageVector = Icons.Filled.FilterAlt,
+                        contentDescription = "Filter"
+                    )
                 }
-                ExportMenu(songDao, songScoreDao, artistDao, navController.context) {
-                    showMenu.value = false
+
+                // メニューボタン
+                IconButton(onClick = { showMenu.value = true }) {
+                    Icon(Icons.Filled.MoreVert, contentDescription = "メニュー")
                 }
-           }
+                DropdownMenu(
+                    expanded = showMenu.value,
+                    onDismissRequest = { showMenu.value = false }
+                ) {
+                    ImportMenu(songDao, songScoreDao, artistDao, navController.context) {
+                        showMenu.value = false
+                    }
+                    ExportMenu(songDao, songScoreDao, artistDao, navController.context) {
+                        showMenu.value = false
+                    }
+                }
+            }
         }
     )
+
+    if (showSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showSheet = false },
+            sheetState = sheetState,
+            shape = BottomSheetDefaults.ExpandedShape,
+            containerColor = BottomSheetDefaults.ContainerColor,
+            tonalElevation = BottomSheetDefaults.Elevation,
+            scrimColor = BottomSheetDefaults.ScrimColor,
+            dragHandle = { BottomSheetDefaults.DragHandle() },
+            windowInsets = WindowInsets.displayCutout,
+        ) {
+            // Sheet content
+            FilterChip()
+        }
+    }
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FilterChip(
+
+){
+    var selected by remember { mutableStateOf(false) }
+
+    FilterChip(
+        onClick = { selected = !selected },
+        label = { Text("JOY") },
+        selected = selected,
+        leadingIcon = if (selected) {
+            {
+                Icon(
+                    imageVector = Icons.Filled.Done,
+                    contentDescription = null,
+                    modifier = Modifier.size(FilterChipDefaults.IconSize)
+                )
+            }
+        }
+        else { null },
+    )
+}
+
+
 
 private fun generateFileName(): String {
     val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
@@ -91,7 +169,13 @@ private fun generateFileName(): String {
 }
 
 @Composable
-fun ExportMenu(songDao: SongDao, songScoreDao: SongScoreDao, artistDao: ArtistDao, context: Context, onClick: () -> Unit = {}) {
+fun ExportMenu(
+    songDao: SongDao,
+    songScoreDao: SongScoreDao,
+    artistDao: ArtistDao,
+    context: Context,
+    onClick: () -> Unit = {}
+) {
     val filePickerLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult()
     ) { result ->
@@ -162,7 +246,13 @@ fun ExportMenu(songDao: SongDao, songScoreDao: SongScoreDao, artistDao: ArtistDa
 }
 
 @Composable
-fun ImportMenu(songDao: SongDao, songScoreDao: SongScoreDao, artistDao: ArtistDao, context: Context, onClick: () -> Unit = {}) {
+fun ImportMenu(
+    songDao: SongDao,
+    songScoreDao: SongScoreDao,
+    artistDao: ArtistDao,
+    context: Context,
+    onClick: () -> Unit = {}
+) {
     val localDateDeserializer = JsonDeserializer { json, _, _ ->
         LocalDate.parse(json.asJsonPrimitive.asString, DateTimeFormatter.ISO_LOCAL_DATE)
     }
